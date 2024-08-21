263b99d4be63565ac791896b02e08e83fc49d26c
전영호 <youngho.jeon@datamaker.io>
2024-08-21 19:01:19 +0900

    Update Cody.rb

--- Cody.rb
+++ Cody.rb
@@ -2,7 +2,7 @@ class Cody < Formula
   desc "Assistant for quickly passing existing code context to LLMs"
   homepage "https://github.com/yhzion/cody"
   url "https://github.com/yhzion/cody/releases/download/v1.0.0/Cody-1.0.0-universal.dmg"
-  sha256 "5c990f8ee757f320c42a52e3ec5a109ea5bf68dde09f3ee2be94952f999a8571" # Cody-1.0.0-universal.dmg 파일의 정확한 SHA256 해시값을 입력하세요.
+  sha256 "a386708ab8b79d75b20b9822762064ff6c1c913016263ea93ee4c65241fdbe07" # Cody-1.0.0-universal.dmg 파일의 정확한 SHA256 해시값을 입력하세요.
   version "1.0.0"
 
   def install
